83e9d55a87e57eb11ed7fe707e644ceb59e405e5
itsubaki <itsubaki@users.noreply.github.com>
2019-02-09 16:31:21 +0900

    Add read csv

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # csv2ss
 csv to google spreadsheets
+
+## Install
+
+```
+$ go get github.com/itsubaki/csv2ss
+```
--- cmd/csv2ss/main.go
+++ cmd/csv2ss/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
 
 	"github.com/google/uuid"
 	"github.com/itsubaki/csv2ss/internal/googless"
@@ -9,33 +12,65 @@ import (
 )
 
 func main() {
-	gss, derr := googless.Default()
-	if derr != nil {
-		fmt.Println(fmt.Errorf("new spreadsheets client: %v", derr))
+	values, err := Read()
+	if err != nil {
+		fmt.Printf("read: %v\n", err)
 		return
 	}
 
-	id := uuid.Must(uuid.NewRandom())
-	ss, nerr := gss.NewSpreadSheets(id.String())
-	if nerr != nil {
-		fmt.Println(fmt.Errorf("new spreadsheets: %v", nerr))
-		return
-	}
-
-	value := &sheets.ValueRange{
-		Values: [][]interface{}{
-			[]interface{}{1, 2, 3, 4, 5},
-			[]interface{}{6, 7, 8, 9, 0},
-		},
-	}
-
-	res, uerr := gss.Update(ss.SpreadsheetId, "シート1", value)
-	if uerr != nil {
-		fmt.Println(fmt.Errorf("update sheet1: %v", uerr))
+	ss, res, err := Write(values)
+	if err != nil {
+		fmt.Printf("write: %v\n", err)
 		return
 	}
 
 	fmt.Println(ss)
 	fmt.Println(res)
 	return
+}
+
+func Read() (*sheets.ValueRange, error) {
+	stdin, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		return nil, fmt.Errorf("stdin: %v", err)
+	}
+	csv := strings.Split(string(stdin), "\n")
+
+	tmp := [][]string{}
+	for _, line := range csv {
+		tmp = append(tmp, strings.Split(line, ", "))
+	}
+
+	// string -> interface{}
+	val := make([][]interface{}, len(tmp))
+	for i := range tmp {
+		val[i] = make([]interface{}, len(tmp[i]))
+		for j := range tmp[i] {
+			val[i][j] = tmp[i][j]
+		}
+	}
+
+	return &sheets.ValueRange{
+		Values: val,
+	}, nil
+}
+
+func Write(values *sheets.ValueRange) (*sheets.Spreadsheet, *sheets.UpdateValuesResponse, error) {
+	gss, derr := googless.Default()
+	if derr != nil {
+		return nil, nil, fmt.Errorf("new spreadsheets client: %v", derr)
+	}
+
+	id := uuid.Must(uuid.NewRandom())
+	ss, nerr := gss.NewSpreadSheets(id.String())
+	if nerr != nil {
+		return nil, nil, fmt.Errorf("new spreadsheets: %v", nerr)
+	}
+
+	res, uerr := gss.Update(ss.SpreadsheetId, "シート1", values)
+	if uerr != nil {
+		return ss, nil, fmt.Errorf("update sheet1: %v", uerr)
+	}
+
+	return ss, res, nil
 }
